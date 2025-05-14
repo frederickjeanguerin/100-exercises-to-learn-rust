f0868a093e9b09e9bc4690d274579bc23cb452c6
fguerin <frederickjeanguerin@gmail.com>
2025-05-14 09:13:25 -0400

    FIN séries 1 + 2

--- exercises/02_basic_calculator/06_while/src/lib.rs
+++ exercises/02_basic_calculator/06_while/src/lib.rs
@@ -4,7 +4,13 @@ pub fn factorial(n: u32) -> u32 {
     // interprets as "I'll get back to this later", thus
     // suppressing type errors.
     // It panics at runtime.
-    todo!()
+    let mut facto = 1u32;
+    let mut i = 2u32;
+    while i <= n {
+        facto *= i;
+        i += 1;
+    }
+    facto
 }
 
 #[cfg(test)]
