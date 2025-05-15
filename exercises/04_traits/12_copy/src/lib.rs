26b534a8a0fc8973ac3b76e821c88dc4ae4fd9f1
fguerin <frederickjeanguerin@gmail.com>
2025-05-15 11:50:25 -0400

    FIN série 4

--- exercises/04_traits/12_copy/src/lib.rs
+++ exercises/04_traits/12_copy/src/lib.rs
@@ -1,6 +1,7 @@
 // TODO: implement the necessary traits to make the test compile and pass.
 //  You *can't* modify the test.
 
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
 pub struct WrappingU32 {
     value: u32,
 }
@@ -9,6 +10,15 @@ impl WrappingU32 {
     pub fn new(value: u32) -> Self {
         Self { value }
     }
+}
+
+impl std::ops::Add for WrappingU32 {
+    type Output = Self;
+    fn add(self, other: Self) -> Self {
+        Self {
+            value: self.value.wrapping_add(other.value),
+        }
+    }   
 }
 
 #[cfg(test)]
--- exercises/04_traits/14_outro/src/lib.rs
+++ exercises/04_traits/14_outro/src/lib.rs
@@ -8,3 +8,55 @@
 //   It should be possible to print its debug representation.
 //
 // Tests are located in the `tests` folder—pay attention to the visibility of your types and methods.
+
+use std::ops::Add;
+
+#[derive(Debug, PartialEq, Copy, Clone)]
+pub struct SaturatingU16 {
+    value: u16,
+}
+
+impl From<u16> for SaturatingU16 {
+    fn from(value: u16) -> Self {
+        SaturatingU16 { value }
+    }
+}
+
+impl From<&u16> for SaturatingU16 {
+    fn from(value: &u16) -> Self {
+        SaturatingU16 { value: *value }
+    }
+}
+
+impl From<u8> for SaturatingU16 {
+    fn from(value: u8) -> Self {
+        SaturatingU16 { value: value.into() }
+    }
+}
+
+impl From<&u8> for SaturatingU16 {
+    fn from(value: &u8) -> Self {
+        SaturatingU16 { value: (*value).into() }
+    }
+}
+
+impl From<&SaturatingU16> for SaturatingU16 {
+    fn from(value: &SaturatingU16) -> Self {
+        *value
+    }
+}
+
+impl<T> Add<T> for SaturatingU16 
+where T: Into<Self>
+{
+    type Output = Self;
+    fn add(self, rhs: T) -> Self::Output {
+        SaturatingU16 { value: self.value.saturating_add(rhs.into().value) }
+    }
+}
+
+impl PartialEq<u16> for SaturatingU16 {
+    fn eq(&self, other: &u16) -> bool {
+        self.value == *other
+    }
+}
